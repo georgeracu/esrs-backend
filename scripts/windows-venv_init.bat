13177ee9460f54401e79ab83fe84e428cfe1a71e
Ben Carter <benjohn.carter@gmail.com>
2020-02-04 21:59:36 +0000

    Changed styling

--- scripts/windows-venv_init.bat
+++ scripts/windows-venv_init.bat
@@ -1,20 +1,24 @@
 ECHO Go up to root
+
 cd ..
 
 ECHO Install the prereqs
+
 pip install virtualenv
 virtualenv venv
 
 ECHO Start the virtual environment
+
 call venv/Scripts/activate
 
 ECHO Install dependencies in the venv
+
 pip install -r requirements.txt
 
 ECHO Set the entry point
+
 set FLASK_APP=app.py
 
 ECHO Done! You can now start the server with `flask run`
 
-ECHO Enable CMD for use
 cmd /k
