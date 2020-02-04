dcdbea5f2da32b397c27751194ec0c44b3dce09a
Ben Carter <benjohn.carter@gmail.com>
2020-02-04 21:57:32 +0000

    Added to the script to continue using the same terminal added verbosity

--- scripts/windows-venv_init.bat
+++ scripts/windows-venv_init.bat
@@ -1,18 +1,20 @@
-REM Go up to root
+ECHO Go up to root
 cd ..
 
-REM Install the prereqs
+ECHO Install the prereqs
 pip install virtualenv
 virtualenv venv
 
-REM Start the virtual environment
+ECHO Start the virtual environment
 call venv/Scripts/activate
 
-REM Install dependencies in the venv
+ECHO Install dependencies in the venv
 pip install -r requirements.txt
 
-REM Set the entry point
+ECHO Set the entry point
 set FLASK_APP=app.py
 
-ECHO Done! You can now start the server from the venv
-@PAUSE
+ECHO Done! You can now start the server with `flask run`
+
+ECHO Enable CMD for use
+cmd /k
