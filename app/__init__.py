ff6b6e23d604bdd2ee31cf8a2b8acf895a4ac946
Ben Carter <benjohn.carter@gmail.com>
2020-02-08 20:00:59 +0000

    Added JSON return and test case

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,20 @@
 from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello world!'
+
+
+@app.route('/EUS')
+def departures():
+    return jsonify(
+        train="LATE!"
+    )
 
 
 if __name__ == '__main__':
     app.run()
-
--- test/test_app.py
+++ test/test_app.py
@@ -2,12 +2,18 @@ import unittest
 from app import app
 
 
-class BasicTestCase(unittest.TestCase):
+class BasicTests(unittest.TestCase):
     def test_index(self):
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, b'Hello World!')
+
+    def test_json(self):
+        tester = app.test_client(self)
+        response = tester.get('/EUS', content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, {"train": "LATE!"})
 
 
 if __name__ == '__main__':
