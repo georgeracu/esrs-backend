44ba0150d5ebad80b2a547f589cdf18a825dce40
Ben Carter <benjohn.carter@gmail.com>
2020-02-11 15:06:34 +0000

    Added tests

--- test/test_json.py
+++ test/test_json.py
@@ -3,17 +3,13 @@ from app import app
 
 
 class MyTestCase(unittest.TestCase):
-    def test_json(self):
+    def test_good_json(self):
+        """Test that the JSON response is OK and length is greater than 0
+        """
         tester = app.test_client(self)
-        response = tester.get('/EUS', content_type='application/json')
+        response = tester.get('/departures/EUS', content_type='application/json')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(
-            response.data,
-            b'{"etd":"16:30",'
-            b'"station":"London Euston",'
-            b'"std":"16:07"}'
-            b'\n'
-        )
+        self.assert_(len(response.data) > 0)
 
 
 if __name__ == '__main__':
